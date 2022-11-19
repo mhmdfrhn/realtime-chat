33cd6463a0486b5d929fd856a709aa1d46946955
mhmdfrhn <frhn2707@gmail.com>
2022-11-19 10:47:33 +0700

    create login/register component

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { ChannelList, Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import './App.css';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
+
+const apiKey = '9shy79wy9wu6';
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
+
+const App = () => {
+  if (!authToken) return <Auth />;
+
+  return (
+    <div className='app__wrapper'>
+      <Chat client={client} theme='team light'>
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
+
+export default App;
